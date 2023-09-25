35a26a009077a5820141a5ffeb00a360d677cc82
Iñigo moreno Crespo <inigo.morenocrespo@plexus.tech>
2023-09-25 14:57:39 +0200

    my firat commit

---
+++ myProject/myProject/myProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  myProjectApp.swift
+//  myProject
+//
+//  Created by Iñigo Moreno Crespo on 25/9/23.
+//
+
+import SwiftUI
+
+@main
+struct myProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
